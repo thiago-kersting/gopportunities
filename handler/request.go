126d86b848fd221e4ce073cbc1fd60c0b86b657d
Thiago Kersting Puls <thiagokpuls@gmail.com>
2025-03-17 20:23:02 -0300

    feat: add request logic

--- handler/request.go
+++ handler/request.go
@@ -42,3 +42,22 @@ func (r *CreateOpeningRequest) Validate() error {
 
 	return nil
 }
+
+// UpdateOpening
+
+type UpdateOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (r *UpdateOpeningRequest) Validate() error {
+	if r.Role != "" || r.Company != "" || r.Location != "" || r.Link != "" || r.Remote != nil || r.Salary > 0 {
+		return nil
+	}
+
+	return fmt.Errorf("at least one valid field must be provided")
+}
