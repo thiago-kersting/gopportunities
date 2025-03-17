209df038f2d570a88dde29acc601f4e3ebaa6486
Thiago Kersting Puls <thiagokpuls@gmail.com>
2025-03-17 18:01:16 -0300

    feat: add new Logger

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
-import "github.com/thiago-kersting/gopportunities/router"
+import (
+	"github.com/thiago-kersting/gopportunities/config"
+	"github.com/thiago-kersting/gopportunities/router"
+)
 
+var logger config.Logger
 
 func main() {
-	
+	logger = *config.GetLogger("main")
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Config initialization error: %v", err)
+		return
+	}
+
 	// Initialize router
 	router.Initialize()
 }
