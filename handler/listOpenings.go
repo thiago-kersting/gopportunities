6b3a8e9d3630b47a2ce79ec9cbb2d31d271a5549
Thiago Kersting Puls <thiagokpuls@gmail.com>
2025-03-17 20:45:46 -0300

    docs: add documentation for list openings

--- handler/listOpenings.go
+++ handler/listOpenings.go
@@ -7,6 +7,16 @@ import (
 	"github.com/thiago-kersting/gopportunities/schemas"
 )
 
+// @BasePath /api/v1
+
+// @Summary Show openings
+// @Description Show jobs openings
+// @Tags Openings
+// @Accept json
+// @Produce json
+// @Success 200 {object} ListOpeningsResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /openings [get]
 func ListOpeningsHandler(ctx *gin.Context) {
 	openings := []schemas.Opening{}
 
